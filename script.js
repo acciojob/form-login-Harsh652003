e80f467bbb1ef7d3c0007a367f7a3695815de5fb
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-05 01:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
 
+ 
+    const firstName = document.getElementsByName("fname")[0].value;
+    const lastName = document.getElementsByName("lname")[0].value;
+
+    
+    const alertMessage = `${firstName} ${lastName}`;
+
+    alert(alertMessage);
 }
+
